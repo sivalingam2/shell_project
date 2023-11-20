ba7244c2f5ff6fcb3406896cd89c644d48650a80
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-20 22:23:46 +0530

    practice

--- mysql.sh
+++ mysql.sh
@@ -5,21 +5,21 @@ if [ -z "${mysql_password}" ]; then
   echo "input password missing"
   exit1
 fi
-echo -e "\e[31m >>>> copy repo files <<<<\e[0m"
+echo -e "\e[32m >>>> copy repo files <<<<\e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 exit_status
-echo -e "\e[31m >>>> disable my sql <<<<\e[0m"
+echo -e "\e[32m >>>> disable my sql <<<<\e[0m"
 dnf module disable mysql -y &>>${log}
 exit_status
-echo -e "\e[31m >>>> install mysql <<<<\e[0m"
+echo -e "\e[32m >>>> install mysql <<<<\e[0m"
 dnf install mysql-community-server -y &>>${log}
 exit_status
-echo -e "\e[31m >>>> enable mysql <<<<\e[0m"
+echo -e "\e[32m >>>> enable mysql <<<<\e[0m"
 systemctl enable mysqld &>>${log}
 exit_status
-echo -e "\e[31m >>>> start mysql <<<<\e[0m"
+echo -e "\e[32m >>>> start mysql <<<<\e[0m"
 systemctl start mysqld &>>${log}
 exit_status
-echo -e "\e[31m >>>> set root password <<<<\e[0m"
+echo -e "\e[32m >>>> set root password <<<<\e[0m"
 mysql_secure_installation --set-root-pass ${mysql_password} &>>${log}
 exit_status
